60f995e835f68e38e82b7cda4e169b96e8f3b5ee
Felipe Santos <felipecassiors@gmail.com>
2024-06-05 15:33:03 -0300

    Change indentation to 2 spaces

--- get_cpus.sh
+++ get_cpus.sh
@@ -3,53 +3,53 @@
 set -eu
 
 if [ "${DEBUG:-}" = true ]; then
-    set -x
+  set -x
 fi
 
 verbose() {
-    if [ "${VERBOSE:-}" = true ]; then
-        echo "VERBOSE(get_cpus.sh):" "$@" >&2
-    fi
+  if [ "${VERBOSE:-}" = true ]; then
+    echo "VERBOSE(get_cpus.sh):" "$@" >&2
+  fi
 }
 
 warning() {
-    echo "WARNING(get_cpus.sh):" "$@" >&2
+  echo "WARNING(get_cpus.sh):" "$@" >&2
 }
 
 if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-    verbose "cgroup v2 detected."
-    if [ -f /sys/fs/cgroup/cpu.max ]; then
-        read -r quota period </sys/fs/cgroup/cpu.max
-        if [ "$quota" = "max" ]; then
-            verbose "No CPU limits set."
-            unset quota period
-        fi
-    else
-        warning "/sys/fs/cgroup/cpu.max not found. Falling back to /proc/cpuinfo."
+  verbose "cgroup v2 detected."
+  if [ -f /sys/fs/cgroup/cpu.max ]; then
+    read -r quota period </sys/fs/cgroup/cpu.max
+    if [ "$quota" = "max" ]; then
+      verbose "No CPU limits set."
+      unset quota period
     fi
+  else
+    warning "/sys/fs/cgroup/cpu.max not found. Falling back to /proc/cpuinfo."
+  fi
 else
-    verbose "cgroup v1 detected."
+  verbose "cgroup v1 detected."
 
-    if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ] && [ -f /sys/fs/cgroup/cpu/cpu.cfs_period_us ]; then
-        quota=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
-        period=$(cat /sys/fs/cgroup/cpu/cpu.cfs_period_us)
+  if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ] && [ -f /sys/fs/cgroup/cpu/cpu.cfs_period_us ]; then
+    quota=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+    period=$(cat /sys/fs/cgroup/cpu/cpu.cfs_period_us)
 
-        if [ "$quota" = "-1" ]; then
-            verbose "No CPU limits set."
-            unset quota period
-        fi
-    else
-        warning "/sys/fs/cgroup/cpu/cpu.cfs_quota_us or /sys/fs/cgroup/cpu/cpu.cfs_period_us not found. Falling back to /proc/cpuinfo."
+    if [ "$quota" = "-1" ]; then
+      verbose "No CPU limits set."
+      unset quota period
     fi
+  else
+    warning "/sys/fs/cgroup/cpu/cpu.cfs_quota_us or /sys/fs/cgroup/cpu/cpu.cfs_period_us not found. Falling back to /proc/cpuinfo."
+  fi
 fi
 
 if [ -n "${quota:-}" ] && [ -n "${period:-}" ]; then
-    cpus=$((quota / period))
-    if [ "$cpus" -eq 0 ]; then
-        cpus=1
-    fi
+  cpus=$((quota / period))
+  if [ "$cpus" -eq 0 ]; then
+    cpus=1
+  fi
 else
-    cpus=$(grep -c processor /proc/cpuinfo)
+  cpus=$(grep -c processor /proc/cpuinfo)
 fi
 
 verbose "CPUs:"
